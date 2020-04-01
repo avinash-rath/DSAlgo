e76c3b8687ad1f27622d5d5008c21653601e59f8
Avinash Rath <avinash.rrath@gmail.com>
2020-04-01 21:47:44 +0530

    Update single_number.py

--- leetcode/single_number.py
+++ leetcode/single_number.py
@@ -7,6 +7,27 @@ class Solution:
       # now n^x^n = (n^n)^x = x. Thus in the end all we 
       # get is the number that isn't repeated. (This works 
       # because it is told that the numbers are repeated twice.)
-      
-      
+      return reduce(lambda x,y:x^y,nums)
+  '''
+This takes a lot of space and is slow
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        mymap = dict()
+        for i in nums :
+          if i in mymap.keys() :
+            mymap[i] += 1
+          else :
+            mymap[i] = 1
+        for i in mymap :
+          if mymap[i] == 1 : return i
+''' 
+
+'''
+This takes a lot of space too
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return 2*(sum(set(nums))) - sum(nums)
+'''    
       
