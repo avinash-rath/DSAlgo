6f890c7a942b9670fca8eadb7f24b6e23c59d368
Avinash Rath <avinash.rrath@gmail.com>
2020-04-16 22:25:10 +0530

    Create valid_parenthesis_string.py

---
+++ leetcode/valid_parenthesis_string.py
@@ -0,0 +1,44 @@
+'''
+678. Valid Parenthesis String
+Medium
+
+Given a string containing only three types of characters: '(', ')' and '*', write a function to check whether this string is valid. We define the validity of a string by these rules:
+
+    Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+    Any right parenthesis ')' must have a corresponding left parenthesis '('.
+    Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+    '*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string.
+    An empty string is also valid.
+
+Example 1:
+
+Input: "()"
+Output: True
+
+Example 2:
+
+Input: "(*)"
+Output: True
+
+Example 3:
+
+Input: "(*))"
+Output: True
+
+Note:
+
+    The string size will be in the range [1, 100].
+'''
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        if not s: return True
+        left=right=0
+        n = len(s)
+        for i in range(n):
+            left += 1 if s[i] in '(*' else -1
+            right += 1 if s[n-i-1] in '*)' else -1
+            if (left < 0) or (right < 0):
+                return False
+        return True
+        
+
