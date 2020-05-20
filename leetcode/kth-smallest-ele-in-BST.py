32ea5437c01e983640624bec40743afef92c07b8
Avinash Rath <avinash.rrath@gmail.com>
2020-05-20 15:27:39 +0530

    Create kth-smallest-ele-in-BST.py

---
+++ leetcode/kth-smallest-ele-in-BST.py
@@ -0,0 +1,60 @@
+'''
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Example 1:
+
+Input: root = [3,1,4,null,2], k = 1
+   3
+  / \
+ 1   4
+  \
+   2
+Output: 1
+
+Example 2:
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+       5
+      / \
+     3   6
+    / \
+   2   4
+  /
+ 1
+Output: 3
+
+Follow up:
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        self.k = k
+        self.ans = None
+        
+        def inorder(node):
+            if self.k==0:
+                return
+            
+            if not node:
+                return
+            inorder(node.left)
+            
+            self.k -= 1
+            
+            if self.k==0:
+                self.ans = node.val
+                return
+            inorder(node.right)
+            
+        inorder(root)
+        return self.ans
