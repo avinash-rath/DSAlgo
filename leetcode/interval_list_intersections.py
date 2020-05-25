bc6b3d1ad9c593d9ec9d3d2f98917ee8b122d7f7
Avinash Rath <avinash.rrath@gmail.com>
2020-05-25 23:44:40 +0530

    Update interval_list_intersections.py

--- leetcode/interval_list_intersections.py
+++ leetcode/interval_list_intersections.py
@@ -5,7 +5,8 @@ Return the intersection of these two interval lists.
 
 (Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.  The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval.  For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
 
- 
+  
+  
 
 Example 1:
 
