ff9e146163e4f0c2c3af6caf5025b0b70bd80ba6
Avinash Rath <avinash.rrath@gmail.com>
2020-06-02 23:38:06 +0530

    Create invert_tree.py

---
+++ leetcode/invert_tree.py
@@ -0,0 +1,54 @@
+'''
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+    Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+
+
+'''
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root is None:
+            return root
+        stack = []
+        stack.append(root)
+        while len(stack) > 0:
+            node = stack.pop()
+            if node is None:
+                continue
+            
+            node.left, node.right = node.right, node.left
+            
+            stack.append(node.left)
+            stack.append(node.right)
+        return root
