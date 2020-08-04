905c443a3e91269ba4511c064d05653d5b3038ef
Avinash Rath <avinash.rrath@gmail.com>
2020-08-04 12:51:53 +0530

    Create PowerOf4.java

---
+++ leetcode/java/PowerOf4.java
@@ -0,0 +1,20 @@
+/*
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 5
+Output: false
+Follow up: Could you solve it without loops/recursion?
+*/
+
+class Solution {
+    public boolean isPowerOfFour(int num) {
+      double a=Math.log(num)/Math.log(4);
+      return ( a == (int)a );
+    }
+}
