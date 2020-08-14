80e727cde22c603858896a057c315af388d8255a
Avinash Rath <avinash.rrath@gmail.com>
2020-08-14 05:49:59 +0530

    Create RemoveDups.java

---
+++ CtCI/java/linked_lists/RemoveDups.java
@@ -0,0 +1,31 @@
+package linked_lists;
+import java.util.*;
+
+/**
+ * 2.1
+ * Remove Dups: Write code to remove duplicates from an unsorted linked list.
+ * Follow up
+ * How would you solve this problem is a temporary buffer is not allowed.
+ */
+
+public class RemoveDups {
+    public void remove(Node<Character> head) {
+        if(head==null) return ;
+        Set<Character> set = new HashSet<Character>();
+        Node<Character> pt,prev;
+        prev = head;
+        set.add(head.data);
+        pt= head.next;
+        while(pt!=null){
+            if(set.contains(pt.data)) {
+                prev.next = pt.next;
+                pt.next = null;
+            }
+            else {
+                set.add(pt.data);
+            }
+            prev = prev.next;
+            pt = prev.next;
+        }
+    }
+}
