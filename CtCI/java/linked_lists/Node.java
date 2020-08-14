b4ee4bbe198dd9f09de62e9200787094f9db377c
Avinash Rath <avinash.rrath@gmail.com>
2020-08-14 05:49:37 +0530

    Create LinkedList Node

---
+++ CtCI/java/linked_lists/Node.java
@@ -0,0 +1,18 @@
+package linked_lists;
+
+public class Node<T> {
+    public T data;
+    public Node<T> next;
+    public Node(T data){
+        this.data = data;
+        this.next = null;
+    }
+    public Node() {
+        this.data = null;
+        this.next = null;
+    }
+    public Node(T data, Node<T> next){
+        this.data = data;
+        this.next = next;
+    }
+}
