dd870a04099ba2345251505b06fdda83564753b4
Avinash Rath <avinash.rrath@gmail.com>
2020-08-17 06:57:22 +0530

    Update Palindrome.java

--- CtCI/java/linked_lists/Palindrome.java
+++ CtCI/java/linked_lists/Palindrome.java
@@ -2,6 +2,12 @@ package linked_lists;
 
 import java.util.Stack;
 
+/**
+ * 2.6
+ * 
+ * Implement a function to check if a linked list is a palindrome
+ */
+
 public class Palindrome {
     public boolean isPalindrome(Node<Character> head) {
         Stack<Character> stack = new Stack<Character>();
@@ -13,13 +19,13 @@ public class Palindrome {
         System.out.println(len);
         n = head;
         while(runner <= len) {
+            // skip one char in middle if the length of LL is odd
             if(runner == ((len/2)+1) && len%2 != 0) {
                 // do nothing
             } 
             else if(runner > len/2) {
                 if(stack.pop() != n.data) {return false;}
             }
-            // skip one char in middle if the length of LL is odd
             // add half of LL to stack
             else stack.add(n.data); 
             runner++; 
