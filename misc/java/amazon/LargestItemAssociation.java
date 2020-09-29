dbddbff116d04470acdafdd7f2ba9141a36ee548
Avinash Rath <avinash.rrath@gmail.com>
2020-09-29 17:17:26 +0530

    Update LargestItemAssociation.java

--- misc/java/amazon/LargestItemAssociation.java
+++ misc/java/amazon/LargestItemAssociation.java
@@ -12,6 +12,7 @@ public class LargestItemAssociation {
             if(adj.containsKey(i.first)) {
                 set = adj.get(i.first);
                 set.add(i.second);
+                adj.put(i.second, set);
             } else {
                 set = new HashSet<String>();
                 set.add(i.first);
@@ -21,7 +22,6 @@ public class LargestItemAssociation {
             }
             max = Math.max(set.size(), max);
         }
-        System.out.println(max);
         String key=null;
         int last=0;
         for(String s : adj.keySet()) {
@@ -32,7 +32,6 @@ public class LargestItemAssociation {
                 continue;
             }
             if(size == max) {
-                System.out.println(adj.get(s).size() + " "+ s);
                 if(s.compareTo(key) < 0 || size > last)
                 key = s;
             }
@@ -50,8 +49,8 @@ public class LargestItemAssociation {
     public static void main(String...s) {
         LargestItemAssociation a = new LargestItemAssociation();
         List<PairString> pairs = new ArrayList<>();
-        pairs.add(new PairString("item1","item2"));
         pairs.add(new PairString("item3","item4"));
+        pairs.add(new PairString("item1","item2"));
         pairs.add(new PairString("item4","item5"));
         System.out.println(a.findLargest(pairs));
     }
