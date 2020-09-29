b896cc5fecf1c823f0ce11471b8d908a737cd4d7
Avinash Rath <avinash.rrath@gmail.com>
2020-09-29 18:54:34 +0530

    Update LargestItemAssociation.java

--- misc/java/amazon/LargestItemAssociation.java
+++ misc/java/amazon/LargestItemAssociation.java
@@ -1,3 +1,34 @@
+/*
+    In order to improve customer experience, Amazon has developed a system to provide recommendations 
+    to the customers regarding the items they can purchase. Based on historical customer purchase information,
+    an item association can be defined as - if an item A is ordered by a customer, then item B is also likely
+    to be ordered by the same customer. All items that are linked together by an item association can be 
+    considered to be in the same group, an item without any association to any other item can be considered
+    to be in its own item association group of size 1.
+
+    Given a list of item association relationships, write an algorithm that outputs the largest item association
+    group. If two groups have the same number of items then select the group which contains the item that appears 
+    first in lexicographic order.
+
+    INPUT
+    The input to the method consistsof an argument-
+    itemAssociation, a list containing pairs of strings representing the items that are ordered together.
+
+    OUTPUT
+    Return a list of strings representing the largest item association group, sorted lexicographically.
+
+    EXAMPLE
+    Input:
+    itemAssociation:
+    [[item1,item2],
+     [item3,item4],
+     [item4,item5]
+    ]
+
+    Output:
+    [item3, item4, item5]
+*/
+
 package amazon;
 
 import java.util.*;
