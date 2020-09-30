95a6fc13f2bce30f6277bbdfcf36f0e8da119920
Avinash Rath <avinash.rrath@gmail.com>
2020-09-30 23:37:05 +0530

    Create NumWaysToCreateANumber.java

---
+++ misc/java/amazon/NumWaysToCreateANumber.java
@@ -0,0 +1,29 @@
+package amazon;
+
+import java.util.*;
+
+public class NumWaysToCreateANumber {
+    public int numWays(int m, int n) {
+        int ans = 0;
+        int tempN = n;
+        int lastMin = 1;
+        Stack<Integer> stack = new Stack<Integer>();
+        while(m-->0) {
+            stack.push(tempN);
+            tempN /= 2;
+        }
+        ans = stack.pop() - lastMin+1;
+        while(!stack.isEmpty()) {
+            int hi = stack.pop();
+            lastMin *= 2;
+            ans *= (hi-lastMin + 1);
+            System.out.println(hi+" "+lastMin+" "+ans);
+        }
+        return ans;
+    }
+
+    public static void main(String ...s) {
+        NumWaysToCreateANumber test = new NumWaysToCreateANumber();
+        System.out.println(test.numWays(3,9));
+    }
+}
