1c49afe4ba6fdc53c6755b61ba18a8ae1bf4628a
Avinash Rath <avinash.rrath@gmail.com>
2021-09-07 01:36:55 +0530

    Add ClimbingStairs.java

---
+++ leetcode/java/ClimbingStairs.java
@@ -0,0 +1,49 @@
+package leetcode.java;
+
+/**
+ * You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45
+ */
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        int[] dp = new int [n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i < n+1; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+    int memo[] = new int[46]; // given constrains 0 <= n <= 45
+    public int climbStairsRec(int n) {
+        if(n < 2) return 1;
+        if(memo[n] > 0) return memo[n];
+        memo[n] = climbStairsRec(n-1) + climbStairsRec(n-2);
+        return memo[n];
+    }
+}
