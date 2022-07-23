b8b0fbd365136b236ee4de5c8cc167e73fb06568
Avinash Rath <avinash.rrath@gmail.com>
2022-07-23 20:41:04 +0530

    Add print_leetcode_tree.py util

---
+++ utils/print_leetcode_tree.py
@@ -0,0 +1,49 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    def __repr__(self):
+        return 'TreeNode({})'.format(self.val)
+    
+def deserialize(string):
+    if string == '{}':
+        return None
+    nodes = [None if val == 'null' else TreeNode(int(val))
+             for val in string.strip('[]{}').split(',')]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids: node.left  = kids.pop()
+            if kids: node.right = kids.pop()
+    return root
+
+def drawtree(root):
+    def height(root):
+        return 1 + max(height(root.left), height(root.right)) if root else -1
+    def jumpto(x, y):
+        t.penup()
+        t.goto(x, y)
+        t.pendown()
+    def draw(node, x, y, dx):
+        if node:
+            t.goto(x, y)
+            jumpto(x, y-20)
+            t.write(node.val, align='center', font=('Arial', 12, 'normal'))
+            draw(node.left, x-dx, y-60, dx/2)
+            jumpto(x, y-20)
+            draw(node.right, x+dx, y-60, dx/2)
+    import turtle
+    t = turtle.Turtle()
+    t.speed(0); turtle.delay(0)
+    h = height(root)
+    jumpto(0, 30*h)
+    draw(root, 0, 30*h, 40*h)
+    t.hideturtle()
+    turtle.mainloop()
+    
+if __name__ == '__main__':
+    #drawtree(deserialize('[1,2,3,null,null,4,null,null,5]'))
+    t = '[1,null,10,12,13,4,6,null,15,null,null,5,11,null,2,14,7,null,8,null,null,null,9,3]'
+    drawtree(deserialize(t))
